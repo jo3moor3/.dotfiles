4030a259b007a67f5e4bd0adb19e50feb4332409
jomor <jo3moore@gmail.com>
2024-03-18 22:41:05 -0400

    ranger - ensured executable

--- apps/ranger/ranger.nix
+++ apps/ranger/ranger.nix
@@ -7,19 +7,35 @@
     ### Core configurations ###
     ".config/ranger/rc.conf".source = ./rc.conf; # config and keybindings
     ".config/ranger/rifle.conf".source = ./rifle.conf; # launcher
-    ".config/ranger/commands.py".source = ./commands.py; # modules
-
-    ### Color schemes ###
-    ".config/ranger/colorschemes/dracula.py".source = ./colorschemes/dracula.py;
-    ".config/ranger/colorschemes/__init__.py".source =
-      ./colorschemes/__init__.py;
-    ### Plugins ###
-    ".config/ranger/plugins/__init__.py".source = ./plugins/__init__.py;
-    ".config/ranger/plugins/zoxide/__init__.py".source =
-      ./plugins/zoxide/__init__.py;
-    ".config/ranger/plugins/ranger_devicons/__init__.py".source =
-      ./plugins/ranger_devicons/__init__.py;
-    ".config/ranger/plugins/ranger_devicons/devicons.py".source =
-      ./plugins/ranger_devicons/devicons.py;
+  };
+  home.file.".config/ranger/commands.py" = {
+    source = ./commands.py; # modules
+    executable = true;
+  };
+  ### Color schemes ###
+  home.file.".config/ranger/colorschemes/dracula.py" = {
+    source = ./colorschemes/dracula.py;
+    executable = true;
+  };
+  home.file.".config/ranger/colorschemes/__init__.py" = {
+    source = ./colorschemes/__init__.py;
+    executable = true;
+  };
+  ### Plugins ###
+  home.file.".config/ranger/plugins/__init__.py" = {
+    source = ./plugins/__init__.py;
+    executable = true;
+  };
+  home.file.".config/ranger/plugins/zoxide/__init__.py" = {
+    source = ./plugins/zoxide/__init__.py;
+    executable = true;
+  };
+  home.file.".config/ranger/plugins/ranger_devicons/__init__.py" = {
+    source = ./plugins/ranger_devicons/__init__.py;
+    executable = true;
+  };
+  home.file.".config/ranger/plugins/ranger_devicons/devicons.py" = {
+    source = ./plugins/ranger_devicons/devicons.py;
+    executable = true;
   };
 }
