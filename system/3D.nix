c29d4adac6a329a6574b5f9b5ef46ac37000570c
jomor <jo3moore@gmail.com>
2024-03-22 00:32:45 -0400

    godot

--- system/3D.nix
+++ system/3D.nix
@@ -2,11 +2,16 @@
 
 {
   environment.systemPackages = with pkgs; [
+    ### DCC ###
     blender # the unstoppable foss force in 3D graphics
     #houdini # Procedural 3D powerhouse, plays well with linux
+    ### ENGINES ###
+    godot_4
 
     ### 3D CONNEXION SPACEMOUSE ###
     spacenavd
+    spnavcfg
+    libspnav
   ];
   hardware.spacenavd.enable = true;
 }
