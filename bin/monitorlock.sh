b04f63962697641da604f2962efffa137da0b2b1
jomor <jo3moore@gmail.com>
2024-08-15 15:54:50 -0400

    monitor lock fix

--- bin/monitorlock.sh
+++ bin/monitorlock.sh
@@ -8,9 +8,9 @@ if [[ -f ${state} ]]; then
 fi
 
 if [[ ${booleanvalue} == "true" ]]; then
-     wlr-randr --output HDMI-A-1 --pos 0,0
-     echo "false" > ${state}
+     wlr-randr --output HDMI-A-1 --pos 1920,0
+     echo "false" >${state}
 else
      wlr-randr --output HDMI-A-1 --pos 2000,2000
-     echo "true" > ${state}
+     echo "true" >${state}
 fi
